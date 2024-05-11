553147428d6e7780e825c475cee7c4667e6f02dc
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2024-05-11 11:55:10 -0300

    a

--- src/srcBackAlmoxarifadoASPCore/AlmoxarifadoServices/GrupoService.cs
+++ src/srcBackAlmoxarifadoASPCore/AlmoxarifadoServices/GrupoService.cs
@@ -1,6 +1,7 @@
 ﻿using AlmoxarifadoDomain.Models;
 using AlmoxarifadoInfrastructure.Data.Interfaces;
 using AlmoxarifadoServices.DTO;
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,42 @@ namespace AlmoxarifadoServices
 {
     public class GrupoService
     {
+        //instalaer o pacote do autoMapper
         private readonly IGrupoRepository _grupoRepository;
-
+        private readonly MapperConfiguration mapperConfiguration;
         public GrupoService(IGrupoRepository pGrupoRepository)
         {
             _grupoRepository = pGrupoRepository;
+            mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Grupo, GrupoGetDTO>();
+                cfg.CreateMap<GrupoGetDTO, Grupo>();
+            });
         }
 
-        public List<Grupo> ObterTodosProdutos()
+        public List<GrupoGetDTO> ObterTodosProdutos()
         {
-            return _grupoRepository.ObterTodosGrupos();
+            //exemplo sem alto mapper
+
+            /*var dados = _grupoRepository.ObterTodosGrupos();
+            List<GrupoGetDTO> lista = new List<GrupoGetDTO>();
+            foreach (var item in dados)
+            {
+                GrupoGetDTO Model = new GrupoGetDTO
+                {
+                    ID_GRU = item.ID_GRU,
+                    NOME_GRU = item.NOME_GRU,
+                    SUGESTAO_GRU = item.SUGESTAO_GRU
+                };
+                lista.Add(Model);
+            }
+            return lista;*/
+
+            //exemplo com alto mapper
+
+            var mapper = mapperConfiguration.CreateMapper();
+
+            return mapper.Map<List<GrupoGetDTO>>(_grupoRepository.ObterTodosGrupos());
         }
 
         public Grupo ObterGrupoPorID(int id)
